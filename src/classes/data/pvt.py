05f5293385adc859063f33d0f2a1e0854c24c4ec
Bergthor Thrastarson <bergthor13@ru.is>
2018-02-09 15:11:25 +0000

    Fixes crash with nanoseconds

--- src/classes/data/pvt.py
+++ src/classes/data/pvt.py
@@ -47,6 +47,7 @@ class PVT():
             self.nano = 1000000000+pvt.nano
         else:
             self.nano = pvt.nano
+
         self.fixType = pvt.fixType
         self.flags = PVT_flags(pvt.flags)
         self.flags2 = PVT_flags2(pvt.flags2)
@@ -70,7 +71,12 @@ class PVT():
         self.magAcc = pvt.magAcc
 
     def getDate(self):
-        return datetime(self.year, self.month, self.day, self.hour, self.min, self.sec, round(self.nano/1000))
+        checkedNano = round(self.nano/1000)
+        if checkedNano < 0:
+            checkedNano = 0
+        elif checkedNano > 999999:
+            checkedNano = 999999
+        return datetime(self.year, self.month, self.day, self.hour, self.min, self.sec, checkedNano)
     
     def __str__(self):
         return str(self.iTOW) + ", " + str(self.year) + ", " + str(self.month) + ", " + str(self.day) + ", " + str(self.hour) + ", " + str(self.min) + ", " + str(self.sec) + ", " + ", " + str(self.tAcc) + ", " + str(self.nano) + ", " + str(self.fixType) + ", " + ", " + str(self.numSv) + ", " + str(self.lon) + ", " + str(self.lat) + ", " + str(self.height) + ", " + str(self.hMSL) + ", " + str(self.hAcc) + ", " + str(self.vAcc) + ", " + str(self.velN) + ", " + str(self.velE) + ", " + str(self.velD) + ", " + str(self.gSpeed) + ", " + str(self.headMot) + ", " + str(self.sAcc) + ", " + str(self.headAcc) + ", " + str(self.pDop) + ", " + str(self.headVeh) + ", " + str(self.magDec) + ", " + str(self.magAcc)
