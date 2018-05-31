bcf175ec7c466294de903f4e95be1b27bf8bf331
Bergthor Thrastarson <bergthor13@ru.is>
2018-05-31 16:03:48 +0000

    Removes prints

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 """
 App
 """
-print("Importing OS")
-
 import os
-print("Importing GPS Application")
 from classes.gps_application import GpsApplication
-print("Importing Constants")
 import constants
 
 abspath = os.path.abspath(__file__)
